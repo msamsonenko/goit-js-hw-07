d709f90eb4e73b330f23ccc3ba769241339cb18f
samsonenkomax87 <samsonenko.max@gmail.com>
2021-11-18 16:48:35 +0200

    almost ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,45 +1,60 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-{
-  /* <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</div> */
-}
-
 const galleryContainer = document.querySelector(".gallery");
+const instance = basicLightbox.create(`
+  <div class="modal">
+      <img id="original-image">
+  </div>
+`)
 
 galleryContainer.addEventListener("click", onImageClick);
 
 function onImageClick(event) {
-  if (event.target.nodeName !== "IMAGE") {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
     return;
   }
+  instance.show()
+
+  const modalImg = document.querySelector('#original-image');
+  modalImg.src = event.target.dataset.source;
+ modalImg.alt = event.target.alt;
+
+  addListenerOnModal();
 }
 
-const markup = galleryItems.map(({ preview, original, description }) => {
+// function showOriginalImg(src, alt){
+//   const instance = basicLightbox.create(`
+//   <div class="modal">
+//       <img src="${src}" alt="${alt}">
+//   </div>
+// `,{closable: true})
+// instance.show()
+// console.log(instance.element());
+// // console.log(instance.visible())
+// // instance.close();
+
+// }
+
+function addListenerOnModal(){
+  const modalWindow = document.querySelector('.modal');
+  modalWindow.addEventListener("click", onModalWindowClick);
+}
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
   const galleryItemEl = document.createElement("div");
   galleryItemEl.classList.add("gallery__item");
-  console.log(galleryItemEl);
 
   const galleryLinkEl = document.createElement("a");
   galleryLinkEl.classList.add("gallery__link");
   galleryLinkEl.href = original;
-  console.log(galleryLinkEl);
 
   const galleryImgEl = document.createElement("img");
   galleryImgEl.classList.add("gallery__image");
   galleryImgEl.alt = description;
   galleryImgEl.src = preview;
   galleryImgEl.dataset.source = original;
-  console.log(galleryImgEl);
 
   galleryLinkEl.appendChild(galleryImgEl);
   galleryItemEl.appendChild(galleryLinkEl);
@@ -47,6 +62,10 @@ const markup = galleryItems.map(({ preview, original, description }) => {
   return galleryItemEl;
 });
 
-galleryContainer.append(...markup);
+galleryContainer.append(...galleryMarkup);
+
+function onModalWindowClick(){
+instance.close();
+}
 
 console.log(galleryItems);
