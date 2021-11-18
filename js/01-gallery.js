8fac5b611bc104fdfa0c45df98b3035b02fb12e7
Maxim Samsonenko <samsonenko.max@gmail.com>
2021-11-18 21:43:52 +0200

    finished task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,66 +6,62 @@ const instance = basicLightbox.create(`
   <div class="modal">
       <img id="original-image">
   </div>
-`)
+`);
 
 galleryContainer.addEventListener("click", onImageClick);
 
 function onImageClick(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-  instance.show()
+	event.preventDefault();
+	if (event.target.nodeName !== "IMG") {
+		return;
+	}
+	instance.show();
 
-  const modalImg = document.querySelector('#original-image');
-  modalImg.src = event.target.dataset.source;
- modalImg.alt = event.target.alt;
+	const modalImg = document.querySelector("#original-image");
+	modalImg.src = event.target.dataset.source;
+	modalImg.alt = event.target.alt;
 
-  addListenerOnModal();
+	addListenerOnModal();
 }
 
-// function showOriginalImg(src, alt){
-//   const instance = basicLightbox.create(`
-//   <div class="modal">
-//       <img src="${src}" alt="${alt}">
-//   </div>
-// `,{closable: true})
-// instance.show()
-// console.log(instance.element());
-// // console.log(instance.visible())
-// // instance.close();
-
-// }
-
-function addListenerOnModal(){
-  const modalWindow = document.querySelector('.modal');
-  modalWindow.addEventListener("click", onModalWindowClick);
+function addListenerOnModal() {
+	const modalWindow = document.querySelector(".modal");
+	modalWindow.addEventListener("click", onModalWindowClick);
+	window.addEventListener("keydown", onEscapeBtnPress);
 }
 
 const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
-  const galleryItemEl = document.createElement("div");
-  galleryItemEl.classList.add("gallery__item");
+	const galleryItemEl = document.createElement("div");
+	galleryItemEl.classList.add("gallery__item");
 
-  const galleryLinkEl = document.createElement("a");
-  galleryLinkEl.classList.add("gallery__link");
-  galleryLinkEl.href = original;
+	const galleryLinkEl = document.createElement("a");
+	galleryLinkEl.classList.add("gallery__link");
+	galleryLinkEl.href = original;
 
-  const galleryImgEl = document.createElement("img");
-  galleryImgEl.classList.add("gallery__image");
-  galleryImgEl.alt = description;
-  galleryImgEl.src = preview;
-  galleryImgEl.dataset.source = original;
+	const galleryImgEl = document.createElement("img");
+	galleryImgEl.classList.add("gallery__image");
+	galleryImgEl.alt = description;
+	galleryImgEl.src = preview;
+	galleryImgEl.dataset.source = original;
 
-  galleryLinkEl.appendChild(galleryImgEl);
-  galleryItemEl.appendChild(galleryLinkEl);
+	galleryLinkEl.appendChild(galleryImgEl);
+	galleryItemEl.appendChild(galleryLinkEl);
 
-  return galleryItemEl;
+	return galleryItemEl;
 });
 
 galleryContainer.append(...galleryMarkup);
 
-function onModalWindowClick(){
-instance.close();
+function onModalWindowClick() {
+	window.removeEventListener("keydown", onEscapeBtnPress);
+	instance.close();
 }
 
+function onEscapeBtnPress(event) {
+	if (event.code !== "Escape") {
+		return;
+	}
+	window.removeEventListener("keydown", onEscapeBtnPress);
+	instance.close();
+}
 console.log(galleryItems);
